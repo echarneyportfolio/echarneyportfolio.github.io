d1d2898c49e11bbb9141d37b1dd5b37ca5a6e49a
Elliot Charney <elliot.charney@ithaka.org>
2016-10-02 12:14:13 -0400

    don't scroll if it would look funny

--- styles/index.js
+++ styles/index.js
@@ -4,23 +4,30 @@ var hide_lists = function(cb) {
     $('#posts-btn').removeClass('disabled');
     $('#projects-btn').removeClass('disabled')
 };
+var scroll = function(id) {
+  var position = $(window).scrollTop();
+  var height = $(window).height();
+  console.log(position, height);
+  if(position < 60 && height < 650) {
+    $('html, body').animate({
+        scrollTop: $(id).offset().top
+    }, 2000);
+  }
+}
 var show_projects = function() {
     $('#posts-btn').removeClass('disabled');
     $('#posts').fadeOut(300, function() {
         $('#projects').fadeIn(300)
     });
-    $('#projects-btn').addClass('disabled')
-    $('html, body').animate({
-        scrollTop: $("#projects").offset().top
-    }, 2000);
+    $('#projects-btn').addClass('disabled');
+    scroll('#projects');
 };
+
 var show_posts = function() {
     $('#projects-btn').removeClass('disabled');
     $('#projects').fadeOut(function() {
         $('#posts').fadeIn(300)
     });
-    $('#posts-btn').addClass('disabled')
-    $('html, body').animate({
-        scrollTop: $("#posts").offset().top
-    }, 2000);
+    $('#posts-btn').addClass('disabled');
+    scroll('#posts');
 };
