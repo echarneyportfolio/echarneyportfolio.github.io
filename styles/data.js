269b81b2a2e3aa9d1b69262ee97cf0ec566964dd
Elliot Charney <elliotcharney@gmail.com>
2020-01-13 11:26:28 -0800

    bubble diagram for skillset

---
+++ styles/data.js
@@ -0,0 +1,68 @@
+let data_two = [
+  {
+    cat: 'pylib',
+    values: [
+      {name: 'Django', value: 100},
+      {name: 'Flask', value: 100}
+    ]
+  },
+  {
+    cat: 'jslib',
+    values: [
+      {name: 'webpack', value: 80},
+      {name: 'Redux', value: 10},
+      {name: 'React.js', value: 40},
+      {name: 'Vue.js', value: 60},
+      {name: 'Node.js', value: 60},
+    ]
+  },
+  {
+    cat: 'workflow',
+    values: [
+      {name: 'Mobile first', value: 40},
+      {name: 'Agile', value: 75}
+    ]
+  },
+  {
+    cat: 'tooling',
+    values: [
+      {name: 'Git', value: 80},
+      {name: 'JetBrains', value: 60},
+      {name: 'Jira', value: 60},
+      {name: 'AWS', value: 40},
+      {name: 'Heroku', value: 70}
+    ]
+  },
+  {
+    cat: 'uilib',
+    values: [
+      {name: 'jQuery', value: 50},
+      {name: 'Bootstrap', value: 90},
+      {name: 'Foundation', value: 90}
+    ]
+  },
+  {
+    cat: 'language',
+    values: [
+      {name: 'SASS', value: 80},
+      {name: 'JavaScript', value: 100},
+      {name: 'HTML / CSS', value: 100},
+      {name: 'Python', value: 100},
+      {name: 'PHP', value: 30},
+      {name: 'Java', value: 20},
+      {name: 'C++', value: 10},
+      {name: 'SQL', value: 50},
+      {name: 'Ruby', value: 30}
+    ]
+  },
+  {
+    cat: 'platform',
+    values: [
+      {name: 'Docker', value: 10},
+      {name: 'SquareSpace', value: 40},
+      {name: 'WordPress', value: 50},
+      {name: 'Drupal', value: 40},
+      {name: 'Unix', value: 50}
+    ]
+  }
+]
